cf5bf7727e72fbda8f8ae5c9816e69d7c75ee7a6
“Dan” <dlspy@hotmail.com>
2020-10-26 16:43:32 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,341 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+import numpy as np
+
+import gr2
+
+pio.templates.default = 'gridon'
+
+# Initiate app
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
+server = app.server
+colors = {
+    'background': 'white',
+    'header': 'black',
+    'text1': 'black',
+    'text2': '#3C85CC'
+}
+markdown_text1 = '''
+
+'''
+
+
+markdown_text2 = '''
+
+'''
+layout = dict(
+    autosize=True,
+    automargin=True,
+    margin=dict(
+        l=30,
+        r=30,
+        b=20,
+        t=40
+    ),
+    hovermode="closest",
+    plot_bgcolor="#F9F9F9",
+    paper_bgcolor="#F9F9F9",
+    legend=dict(font=dict(size=10), orientation='h'),
+    title='Satellite Overview',
+    mapbox=dict(
+        # accesstoken=mapbox_access_token,
+        style="light",
+        center=dict(
+            lon=-78.05,
+            lat=42.54
+        ),
+        zoom=7,
+    )
+)
+
+app.layout = html.Div(
+    [
+        dcc.Store(id='aggregate_data'),
+        html.Div(
+            [
+
+                html.Div(
+                    [
+                        html.Div(
+                            [
+
+                                html.H6(
+                                    id="experiment_desc",
+                                    className="info_text"
+                                ),
+                                html.H6(
+                                    "Try adjusting the parameters below, can you find the best outcome?",
+                                    id="static_experiment_desc",
+                                    className="info_text",
+
+                                ),
+                                html.Ul(
+                                    [html.Li("What is the optimal bet percentage for these odds?"), html.Li("What happens as you increase number of rounds?")])
+
+                            ],
+                            id="tripleContainer",
+                            className="pretty_container"
+                        ),
+
+
+
+                    ],
+                    id="infoContainer",
+                    className="row description_div"
+                ),
+            ],
+            id="header",
+            className='row',
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [html.Div([
+                        html.H6("Parameters", className="param_title"),
+                        html.P(
+                            'Select the odds of winning the bet',
+                            className="control_label"
+                        ),
+                        dcc.Slider(
+                            id='p-slider',
+                            min=0,
+                            max=100,
+                            step=5,
+                            value=70,
+                            marks={0: '0', 50: '50%', 100: '100%'}
+                        ),
+                        html.P(id='p-slider-output-container',
+                               className="control_label control_output_label"),
+                    ], className="param_section"),
+                        html.Br(),
+
+                        html.P(
+                            'Enter your starting amount of money',
+                            className="control_label"
+                    ),
+                        dcc.Input(
+                            id="input_start", type="number", placeholder="Enter your starting amount of money",
+                            min=10, max=100000, step=10, value=10
+                    ),
+                        html.P(
+                            "Select the % of your money you want to bet each round",
+                            className="control_label"
+                    ),
+                        dcc.Slider(
+                            id='bet-p-slider',
+                            min=0,
+                            max=100,
+                            step=5,
+                            value=80,
+                            marks={0: '0', 50: '50%', 100: '100%'}
+                    ),
+                        html.Div(id='bet-p-slider-output-container',
+                                 className="control_label control_output_label"),
+                        html.P(
+                            'Enter number of rounds to play',
+                            className="control_label"
+                    ),
+                        dcc.Input(
+                            id="input_rounds", type="number", placeholder="Enter number of rounds to play",
+                            min=0, max=10000, step=10, value=200
+                    ),
+
+                        html.P(
+                            'Enter number of trials (How many people are playing in this simulation)',
+                            className="control_label"
+                    ),
+                        dcc.Input(
+                            id="input_trials", type="number", placeholder="Enter number of trials",
+                            min=100, max=500, step=100, value=200
+                    ),
+                        html.Div(
+                            [html.Button('GO', id='go_button', className="go-button", n_clicks=0)], className="go-button-div"),
+
+
+                    ],
+                    className="pretty_container four columns"
+                ),
+                html.Div(
+                    [
+
+                        html.Div(
+                            [
+                                dcc.Graph(
+                                    id='count_graph',
+                                )
+                            ],
+                            id="countGraphContainer",
+                            className="pretty_container"
+                        )
+                    ],
+                    id="rightCol",
+                    className="eight columns"
+                )
+            ],
+            className="row"
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        dcc.Graph(id='main_graph')
+                    ],
+                    className='pretty_container eight columns',
+                ),
+                html.Div(
+                    [
+                        html.H1('Breakdown', id='breakdown_title'),
+                        html.Div([
+                            html.P(id='breakdown_stats')
+                        ]),
+                        html.Div([
+                            html.P(id='breakdown_stats_1')
+                        ]),
+                        html.Div([
+                            html.P(id='breakdown_stats_2')
+                        ]),
+                        html.Div([
+                            html.P(id='breakdown_stats_3')
+                        ]),
+                        html.Div([
+                            html.P(id='breakdown_stats_4')
+                        ])
+                    ],
+                    className='pretty_container four columns',
+                ),
+            ],
+            className='row'
+        ),
+        html.Div(
+            [
+
+                html.Div(
+                    [
+                        html.Div(
+                            [
+
+
+                                html.H2(
+                                    "How to find the best bet percentage",
+                                    id="",
+                                    className="info_text",
+
+                                ),
+                                html.Br(),
+                                html.H4(
+                                    "The Kelly Criterion"),
+                                html.H6(
+                                    "The Kelly Criterion is a mathematical formula that helps investors and gamblers calculate what percentage of their money they should allocate to each investment or bet."),
+                                html.A(
+                                    'Investopedia', href='https://www.investopedia.com/articles/trading/04/091504.asp#:~:text=The%20Kelly%20Criterion%20is%20a,to%20each%20investment%20or%20bet.'),
+                                html.A(
+                                    'Wikipedia', href='https://en.wikipedia.org/wiki/Kelly_criterion'),
+                                html.H6(
+                                    id="kelly_optimal")
+
+                            ],
+                            id="",
+                            className="pretty_container kelly_container"
+                        ),
+
+
+
+                    ],
+                    id="",
+                    className="row description_div kelly_container"
+                ),
+            ],
+
+            className='row',
+        ),
+
+    ],
+    id="mainContainer",
+    style={
+        "display": "flex",
+        "flex-direction": "column"
+    }
+)
+
+
+@app.callback(
+    [dash.dependencies.Output(
+        'p-slider-output-container', 'children'), Output('kelly_optimal', 'children')],
+    [dash.dependencies.Input('p-slider', 'value')])
+def update_p_slider_output(value):
+    kelly_p = round(((value)-(100-value)), 2)
+    if kelly_p <= 0:
+        kelly_p = 0
+    kelly = "The optimal bet percentage, for the current selected odds, is %s%%." % (
+        str(int(kelly_p)))
+    return 'You have selected {}%'.format(value), kelly
+
+
+@app.callback(
+    dash.dependencies.Output('bet-p-slider-output-container', 'children'),
+    [dash.dependencies.Input('bet-p-slider', 'value')])
+def update_bet_p_slider_output(value):
+    return 'You have selected {}%'.format(value)
+
+
+@app.callback(
+    dash.dependencies.Output('experiment_desc', 'children'),
+    [dash.dependencies.Input('input_trials', 'value'), dash.dependencies.Input('input_rounds', 'value'),
+     dash.dependencies.Input('p-slider', 'value'), dash.dependencies.Input('input_start', 'value'), dash.dependencies.Input('bet-p-slider', 'value')])
+def update_exp_desc(v1, v2, v3, v4, v5):
+    return "Imagine %s people independently playing %s rounds of a game with %s%% chance of winning each round. They start with £%s and bet %s%%  of their money each round." % (v1, v2, v3, v4, v5)
+
+
+@app.callback(
+    [
+        Output('count_graph', 'figure'), Output('main_graph', 'figure'),
+        Output('breakdown_stats', 'children'), Output(
+            'breakdown_stats_1', 'children'), Output('breakdown_stats_2', 'children'),
+        Output('breakdown_stats_3', 'children'), Output('breakdown_stats_4', 'children')],
+    [dash.dependencies.Input('go_button', 'n_clicks'), dash.dependencies.State('input_trials', 'value'), dash.dependencies.State('input_rounds', 'value'),
+     dash.dependencies.State('p-slider', 'value'), dash.dependencies.State('input_start', 'value'), dash.dependencies.State('bet-p-slider', 'value')])
+def update_after_go(value, trials, rounds, p, start, bet_p):
+
+    p = p/100
+    bet_p = bet_p/100
+    loading = True
+
+    gr = gr2.GR2(num_trials=trials, games_per_trial=rounds,
+                 win_prob=p, start_amount=start, bet_percent=bet_p)
+    out = gr.simulate()
+    loading = False
+
+    median = gr.median_result(out[-1]).round(2)
+    median_per_round = gr.median_per_round(out)
+    winnings = gr.get_winnings_for_each_trial(out[-1])
+    fig1 = px.line(median_per_round, x=median_per_round["Round"],
+                   y=median_per_round["Median Winnings"], title="Median Winnings by Round")
+
+    fig2 = px.scatter(winnings, title="Winnings After %s Rounds" % (rounds))
+    fig2.update_layout(showlegend=False)
+    fig2.update_xaxes(title_text='Person / Trial')
+    fig2.update_yaxes(title_text='Winnings after {} rounds'.format(rounds))
+
+    top = gr.max_result_formatted(out[-1])
+    first, second, third = '1st place: £' + \
+        top[0], '2nd place: £' + top[1], '3rd place: £' + top[2]
+    # max_result = 'Highest Earner: £' + gr.max_result_formatted(out[-1])
+    median = gr.median_result(out[-1]).round(2)
+    median = gr.format_price(median)
+    median_ret = 'Median Earnings: £' + median
+    lost = gr.percent_lost(out[-1])
+    lost_ret = '%s out of %s players finished with less than they started' % (
+        lost, trials)
+
+    return fig1, fig2, first, second, third, median_ret, lost_ret
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ gr2.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import math
+
+
+class GR2():
+    def __init__(self, num_trials, games_per_trial, win_prob, start_amount, bet_percent):
+
+        self.num_trials = num_trials
+        self.games_per_trial = games_per_trial
+        self.win_prob = win_prob
+        self.start_amount = start_amount
+        self.bet_percent = bet_percent
+
+    def kelly(self, p):
+        return p-(1-p)
+
+    def simulate(self):
+
+        outcomes = np.zeros((self.games_per_trial, self.num_trials))
+        outcomes[0] = self.start_amount
+
+        for i in range(self.num_trials):
+            for j in range(1, self.games_per_trial):
+                res = np.random.binomial(1, self.win_prob)
+
+                if res > 0:
+                    outcomes[j, i] = (1+self.bet_percent) * outcomes[j-1, i]
+                else:
+                    outcomes[j, i] = (1-self.bet_percent) * outcomes[j-1, i]
+        return outcomes
+
+    def num_wins(self, result):
+        wins = sum([1 for i in result if i > self.start_amount])
+        return wins
+
+    def num_wins_above_target(self, result, target):
+
+        wins_above_target = sum([1 for i in result if i > target])
+        return wins_above_target
+
+    def mean_result(self, result):
+        return np.mean(result).round(2)
+
+    def median_result(self, result):
+        return np.median(result).round(2)
+
+    def min_max_result(self, result):
+        return min(result), max(result)
+
+    def median_per_round(self, result):
+        mpr = {i+1: np.median(result[i]).round(2) for i in range(len(result))}
+        return pd.DataFrame(list(mpr.items()), columns=['Round', 'Median Winnings'])
+
+    def get_winnings_for_each_trial(self, result):
+        return pd.DataFrame(result)
+
+    def max_result_formatted(self, result):
+
+        results = sorted(result)[-3:]
+        output = []
+        while results:
+            n = results.pop()
+            #n = max(result)
+            output.append(self.format_price(n))
+        return output
+
+    def percent_lost(self, result):
+
+        lost = sum([1 for i in result if i < self.start_amount])
+        return lost
+
+    def format_price(self, n):
+        if n > 1e6:
+            millnames = ['', ' Thousand',
+                         ' Million', ' Billion', ' Trillion']
+            n = float(n)
+            millidx = max(0, min(len(millnames)-1,
+                                 int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+
+            return('{:.0f}{}'.format(
+                n / 10**(3 * millidx), millnames[millidx]))
+        else:
+            if n < 100:
+                n = round(n, 2)
+            else:
+                n = int(round(n, 0))
+            return "{:,}".format(n)
